d3c7038ffd4c510fc2deb50bd041703a714ab35e
Sandrexa717 <153091202+Sandrexa717@users.noreply.github.com>
2023-12-10 19:57:36 +0400

    Add files via upload

---
+++ day 3/homework/lesson2.py
@@ -0,0 +1,12 @@
+name = "sandro"
+surname = "sakhelashvili"
+height = "172.5"
+age = "13"
+knows_programming = False
+is_chad = True #because learns in GOA
+print((name) + " " + (surname) + " " + (age) + " " + (height))
+name = "nika"
+surname = "mosiashvili"
+height = "1.40"
+age = "12"
+print((name) + " " + (surname) + " " + (age) + " " + (height))
---
+++ day 3/lesson3.classwork.py
@@ -0,0 +1,24 @@
+name = "sandro"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "sandro" არის ცვლადისთვის მნიშვნელობა
+surname = "sakhelashvili"
+print(name)
+#print(name)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+name = "sandro" #ეს არის str (string) ტიპის ცვლადი
+age = 22 #int (integer) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+knows_programming = True #True or False
+is_ugly = False   #snakcase(უბრალოდ წერის სტილი სტანდარტულად)
+isUgly = False    # ჯავასკრიპტული cameLcase
+print(name + " " + surname)
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+print(name + str(age))
